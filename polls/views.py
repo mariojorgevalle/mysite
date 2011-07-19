7e70c877eb04db76e44083e58b0fde1e7c7cef1a
mariojorgevalle <mariojorgevalle@gmail.com>
2011-07-19 11:40:37 -0300

    Finished Tutorial 3.

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,6 @@
-from django.template import Context, loader
 from polls.models import Poll
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.http import Http404
+from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -9,7 +8,8 @@ def index(request):
 
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
--- urls.py
+++ urls.py
@@ -1,12 +1,9 @@
-from django.conf.urls.defaults import patterns, include, url
+from django.conf.urls.defaults import * #patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^polls/$', 'polls.views.index'),
-    (r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    (r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    (r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
-    url(r'^admin/', include(admin.site.urls)),
+    (r'^polls/', include('polls.urls')),
+    (r'^admin/', include(admin.site.urls)),
 )
